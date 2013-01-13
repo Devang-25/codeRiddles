8d49b928dc45783b68b7347405069c397c240645
arcolife <arcshams@gmail.com>
2013-01-13 19:37:25 +0530

    added new src to hackerRank

--- hacker_rank/tictactoe_my.py
+++ hacker_rank/tictactoe_my.py
@@ -1,13 +1,19 @@
 #!/bin/python                                                                                                                                   
-import random
+from random import *
+from string import *
 # Complete the function below to print 2 integers separated by a single space which will be your next move                                      
 def nextMove(player,board):
 #If player is X, I'm the first player.                                                                                                          
 #If player is O, I'm the second player.                                                                                                         
-player = raw_input()
+    print 'fixx'
 #Read the board now. The board is a 3x3 array filled with X, O or _.                                                                            
 board = []
-for i in xrange(0, 3):
-    board.append(raw_input())
-nextMove(player,board)
-
+list=[]         
+for j in xrange(0,3):
+    for i in xrange(0, 3):
+        board.append(raw_input())
+list = join(board,"\n")
+#player = raw_input()
+#nextMove(player,board)
+print list
+print board
