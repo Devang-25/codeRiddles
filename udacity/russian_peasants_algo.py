8af003ad0ac9d1ecd0b40c160d623ba770bb0c52
arcolife <arcshams@gmail.com>
2014-01-16 04:01:45 +0530

    russian_peasants_algo: display execution info

--- udacity/russian_peasants_algo.py
+++ udacity/russian_peasants_algo.py
@@ -4,13 +4,16 @@ def russian(a,b):
     x = a; y = b
     z = 0
     while x > 0:
+        print "x, y, z : ", x,y,z
         if x % 2 == 1: z += y 
-        y <<= 1
-        x >>= 1
+        y <<= 1 # double y
+        x >>= 1 # halve x
+    print
     return z
 
 ip = map(int, raw_input().split())
-print reduce(lambda x,y: russian(x,y), ip)
+ans = reduce(lambda x,y: russian(x,y), ip)
+print "Multiplication: ", ans
 
 '''
 # What reduce() does.. 
