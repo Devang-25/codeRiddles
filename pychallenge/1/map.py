2b212bed1e4be5606cda9b77152eb4b3b8662c7c
arcolife <arcshams@gmail.com>
2014-03-08 07:48:08 +0530

    pychallenge/1: lambda -> chr() *I need some sleep*

--- pychallenge/1/map.py
+++ pychallenge/1/map.py
@@ -2,9 +2,9 @@ from string import maketrans
 
 text = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 
-convert = lambda x: chr(x)
-letters = ''.join(map(convert, xrange(97,123)))
-letters_new = ''.join(map(convert, xrange(99,123)))+'ab'
+#convert = lambda x: chr(x)
+letters = ''.join(map(chr, xrange(97,123)))
+letters_new = ''.join(map(chr, xrange(99,123)))+'ab'
 translation = maketrans(letters, letters_new)
 
 print text
