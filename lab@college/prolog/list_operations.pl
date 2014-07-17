0f5dc97cd4d0d6fb4e5808a12ead48f3b3faf32d
arcolife <arcshams@gmail.com>
2014-07-17 19:34:36 +0530

    list_operations: add reverse, palindrome

--- lab@college/prolog/list_operations.pl
+++ lab@college/prolog/list_operations.pl
@@ -1,5 +1,17 @@
 % -*- Prolog -*-
 
+%% ?- size([a,b,c,d,e], R).
+%% R = 5.
+
+%% ?- delete(d, [a,b,c,d,e], R).
+%% R = [a, b, c, e] 
+
+%% ?- reverse([a,b,c,d,e], R).
+%% R = [e, d, c, b, a].
+
+%% ?- palindrome([1,2,3,2,1]).
+%% true.
+
 size([],0).
 
 size([_|T],R):-
@@ -9,3 +21,11 @@ size([_|T],R):-
 delete(X, [X|T], T).
 delete(X, [H|T], [H|NT]) :- 
     delete(X, T, NT).
+
+reverse([],[]).
+reverse([H|T],L):-
+    reverse(T,R),
+    append(R,[H],L).
+
+palindrome(L):-
+    reverse(L,L).
