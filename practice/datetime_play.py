a3b55ecf8014be36128c1da5a932a26858aaf74f
Archit Sharma <arcsharm@redhat.com>
2015-02-05 14:04:49 +0530

    practice: add datetime object help

---
+++ practice/datetime_play.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+epoch = datetime
+
+# form datetime object from custom date
+x = datetime(2015,1,25,23,59,59)
+# OR
+x = datetime(year=2015, day=25, month=1, hour=23, minute=59, second=59)
+# datetime.datetime(2015, 1, 25, 23, 59, 59)
+
+# datetime to string
+tmp  = str(x)
+# "2015-01-25 23:59:59"
+
+# string to datetime
+dt = datetime.strptime(tmp, "%Y-%m-%d %H:%M:%S")
+# datetime.datetime(2015, 1, 25, 23, 59, 59)
+
+# datetime to UNIX timestamp
+def _normalize_tstamp(dt):
+    td = dt - epoch
+    return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 1e6
+
+_normalize_tstamp(dt)
+# 1422230399.0
+
+# UNIX to timestamp
+datetime.utcfromtimestamp(1422230399.0)
+# datetime.datetime(2015, 1, 25, 23, 59, 59)
