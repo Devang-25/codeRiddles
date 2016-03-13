5f2032fe23adf52a497d14786578800f0c94d960
Archit Sharma <arcsharm@redhat.com>
2016-03-14 04:12:12 +0530

    euler p4: fix lower bound case

--- proj_euler/p4_palindrome_prod.py
+++ proj_euler/p4_palindrome_prod.py
@@ -2,23 +2,39 @@
 
 def find_largest_palindrome(upper, lower):
     _tmp = {}
-    for i in range(upper, lower,-1):
-        flag = lower
+    flag = lower-1
+    for i in range(upper, lower-1, -1):
         if flag <= i:            
             for j in range(i, flag, -1):
                 x = str(i*j)
                 if x == x[::-1]:
-                    flag = j
+                    flag = j-1
                     _tmp[x] = (i,j)
                     break
         else:
             break
     return _tmp
 
-_tmp = find_largest_palindrome(999,99)
-final = [int(i) for i in _tmp.keys()]
-print(max(final), _tmp[str(max(final))])
+# _tmp = find_largest_palindrome(99999,10000)
+# final = [int(i) for i in _tmp.keys()]
+# print(max(final), _tmp[str(max(final))])
+# print(_tmp)
+# print()
 
-_tmp = find_largest_palindrome(99,9)
+_tmp = find_largest_palindrome(9999,1000)
 final = [int(i) for i in _tmp.keys()]
 print(max(final), _tmp[str(max(final))])
+print(_tmp)
+print()
+
+_tmp = find_largest_palindrome(999,100)
+final = [int(i) for i in _tmp.keys()]
+print(max(final), _tmp[str(max(final))])
+print(_tmp)
+print()
+
+_tmp = find_largest_palindrome(99,10)
+final = [int(i) for i in _tmp.keys()]
+print(max(final), _tmp[str(max(final))])
+print(_tmp)
+print()
