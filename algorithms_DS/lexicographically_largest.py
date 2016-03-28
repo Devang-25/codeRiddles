f03a92ec76710698b8814eba512a4e2e5d669121
Archit Sharma <arcsharm@redhat.com>
2016-03-29 00:38:07 +0530

    sort loop-wise for connected vertices sets

--- algorithms_DS/lexicographically_largest.py
+++ algorithms_DS/lexicographically_largest.py
@@ -7,11 +7,22 @@ swapping_indices = [(1,4), (3,4)]
 if __name__=="__main__":
     ip = input("Enter string: ")
     d = dict(enumerate(ip, 1))
-    v = set(); [v.update(i) for i in swapping_indices]
-    x = sorted(operator.itemgetter(*v)(d),
-           reverse=True)
-    for i,j in zip(v,x):
-        d[i] = j
+
+    B = []
+    while swapping_indices:
+        b = swapping_indices[0]; v = set(b); x = [b]
+        for i in swapping_indices[1:]:
+            if set(b) & set(i):
+                v.update(b+i)
+                x.append(i)
+        [swapping_indices.remove(j) for j in x]
+        B.append(v)
+
+    for v in B:
+        x = sorted(operator.itemgetter(*v)(d),
+                   reverse=True)
+        for i,j in zip(v,x):
+            d[i] = j
     
     print(''.join(d.values()))
 
