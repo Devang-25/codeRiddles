947a5d9dc9739da3c55ad60ab09565f8f8e1a866
Archit Sharma <arcsharm@redhat.com>
2016-03-29 00:51:18 +0530

    take swappable vertices as input

--- algorithms_DS/lexicographically_largest.py
+++ algorithms_DS/lexicographically_largest.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
-import operator
+# Usage:
+# $ ./lexicographically_largest.py
+#   Enter string: abdc
+#   Example of swappable indices input (2 pairs): 1,4-3,4
+#   Enter swappable indices in pairs: 1,4-3,4
+#   Answer: dbca
 
-swapping_indices = [(1,4), (3,4)]
+import operator
 
 if __name__=="__main__":
     ip = input("Enter string: ")
+
+    print("Example of swappable indices input (2 pairs): 1,4-3,4")
+    swp_ip = input("Enter swappable indices in pairs: ")
+    swapping_indices = [[int(j) for j in i.split(',')]
+                        for i in swp_ip.split('-')]
+    
     d = dict(enumerate(ip, 1))
 
+    # B contains all loops of connected vertices, if
+    # say each position in string is a vertex. Connected
+    # vertices are swappable. (Permutations)
     B = []
     while swapping_indices:
         b = swapping_indices[0]; v = set(b); x = [b]
@@ -18,11 +32,13 @@ if __name__=="__main__":
         [swapping_indices.remove(j) for j in x]
         B.append(v)
 
+    # for each group of connected loops, swap and
+    # reverse sort letters, to get largest one on leftmost
+    # position in string. Update new positions at the end.
     for v in B:
         x = sorted(operator.itemgetter(*v)(d),
                    reverse=True)
         for i,j in zip(v,x):
             d[i] = j
     
-    print(''.join(d.values()))
-
+    print("Answer: %s" % ''.join(d.values()))
