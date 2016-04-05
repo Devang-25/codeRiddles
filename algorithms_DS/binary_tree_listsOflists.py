92a7998eeae25960e4c678ba2ae1578ace3b2201
Archit Sharma <arcsharm@redhat.com>
2016-04-05 14:08:38 +0530

    update binary tree

--- algorithms_DS/binary_tree_listsOflists.py
+++ algorithms_DS/binary_tree_listsOflists.py
@@ -1,31 +1,5 @@
 #!/usr/bin/env python2
 
-# class tree():
-#     def __init__(self):
-#         '''Initialise the tree'''
-#         self.Data = None
-#         self.Count = 0
-#         self.LeftSubtree = None
-#         self.RightSubtree = None
-
-#     def Insert(self, data):
-#         '''Add an item of data to the tree'''
-#         if self.Data == None:
-#             self.Data = data
-#             self.Count += 1
-#         elif data < self.Data:
-#             if self.LeftSubtree == None:
-#                 # tree is a recurive class definition
-#                 self.LeftSubtree = tree()
-#             # Insert is a recursive function
-#             self.LeftSubtree.Insert(data)
-#         elif data == self.Data:
-#             self.Count += 1
-#         elif data > self.Data:
-#             if self.RightSubtree == None:
-#                 self.RightSubtree = tree()
-#             self.RightSubtree.Insert(data)
-
 def BinaryTree(r):
     return [r, [], []]
 
@@ -59,15 +33,6 @@ def getRightChild(root):
                         
                         
 if __name__ == '__main__':
-
-    # T = tree()
-    # # The root node
-    # T.Insert(raw_input('Enter an alphabet: '))
-    # # Will be put into the left subtree
-    # T.Insert(raw_input('Enter an alphabet: '))
-    # # Will be put into the right subtree
-    # T.Insert(raw_input('Enter an alphabet: '))
-
     r = BinaryTree(3)
     insertLeft(r,4)
     insertLeft(r,5)
