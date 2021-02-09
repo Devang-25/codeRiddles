809e99bd6ec87d9f1efb189eb8d3be9f8be0df3b
arcolife <archit.py@gmail.com>
2021-02-10 03:46:21 +0530

    fix quadratic solution

--- practice/python/math/quadratic_roots.py
+++ practice/python/math/quadratic_roots.py
@@ -3,7 +3,8 @@
 # handles imaginary numbers (sqrt(negative))
 from cmath import sqrt
 
-def quadraticRoots(a,b,c):
+
+def quadraticRoots(a, b, c):
     """
     ax2 + bx + c
 
@@ -15,15 +16,15 @@ def quadraticRoots(a,b,c):
 
     """
     if a != 0:
-        root_1 = (-b + sqrt(b**2 - 4*a*c))/2*a
-        root_2 = (-b + sqrt(b**2 - 4*a*c))/2*a
+        root_1 = (-b + sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+        root_2 = (-b - sqrt(b ** 2 - 4 * a * c)) / (2 * a)
         print(f"Solving for Quadratic: {a}x2 + {b}x + {c}")
         print(root_1, root_2)
     else:
         print(f"Linear equation {b}.x+{c} since a = {a}")
-        print("Single root: ", -c/b)
+        print("Single root: ", -c / b)
 
 
 a, b, c = [int(i) for i in input("Enter with space - a, b, c: ").split()]
-        
+
 quadraticRoots(a, b, c)
